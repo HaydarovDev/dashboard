7ebb4436015871d531d43b9e8cb5f83b1471e158
haydarov_dev <122049418+HaydarovDev@users.noreply.github.com>
2025-08-15 15:23:31 +0500

    feat: sign in finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Signin from "./auth/SignIn/Signin";
+import Home from "./Home";
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
       </Routes>
     </>
--- src/auth/SignIn/Signin.tsx
+++ src/auth/SignIn/Signin.tsx
@@ -1,40 +1,70 @@
+import { Helmet } from "react-helmet-async";
 import logo from "../../assets/images/logo.svg";
 import google from "../../assets/images/google.svg";
 import apple from "../../assets/images/apple.svg";
 import "./Signin.css";
+import EmailIcon from "../../assets/images/email";
+import LockIcon from "../../assets/images/Lock";
+import { Link } from "react-router-dom";
+import EyeIcon from "../../assets/images/eye";
 
 const Signin = () => {
   return (
-    <section>
-      <main>
+    <>
+      <Helmet>
+        <title>DiveSea - Signin</title>
+        <meta name="description" content="Here you can sign in" />
+        <meta property="og:title" content="Signin" />
+      </Helmet>
+      <section>
         <header>
-          <img src={logo} alt="" width={40} />
+          <a href="/" aria-label="company logo">
+            <img src={logo} alt="company logo" width={40} loading="lazy" />
+          </a>
         </header>
-        <article>
-          <div className="auth">
-            <h1>Sign In To DiveSea</h1>
-            <div className="auth-providers">
-              <button>
-                <img src={google} alt="" />
-              </button>
-              <button>
-                <img src={apple} alt="" />
-              </button>
+        <main>
+          <article>
+            <div className="auth">
+              <h1>Sign In To DiveSea</h1>
+              <div className="auth-providers">
+                <button aria-label="google">
+                  <img src={google} alt="apple" loading="lazy" />
+                </button>
+                <button aria-label="apple">
+                  <img src={apple} alt="apple" loading="lazy" />
+                </button>
+              </div>
+              <span>OR</span>
+              <form action="" onSubmit={(e) => e.preventDefault()}>
+                <label htmlFor="email">
+                  <EmailIcon size={25} />
+                  <input type="email" placeholder="Email" id="email" required />
+                </label>
+                <label htmlFor="password">
+                  <LockIcon size={25} />
+                  <input
+                    type="text"
+                    placeholder="Password"
+                    id="password"
+                    required
+                  />
+                  <EyeIcon size={25} />
+                </label>
+                <Link to={"/reser-password"}>Forget Password ?</Link>
+                <button aria-label="signin">Sign in</button>
+                <p>
+                  Create A New Account? <Link to={"/signin"}>Sign Up</Link>
+                </p>
+              </form>
             </div>
-            <span>OR</span>
-            <form action="">
-              <label htmlFor="">
-                <input type="text" />
-              </label>
-              <label htmlFor="">
-                <input type="text" />
-              </label>
-            </form>
-          </div>
-        </article>
-      </main>
-      <main></main>
-    </section>
+          </article>
+        </main>
+        <footer>
+          <Link to={"/privacy"}>Privacy Policy</Link>
+          <p>Copyright 2022</p>
+        </footer>
+      </section>
+    </>
   );
 };
 
