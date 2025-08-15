e3189c4e1ab20dc7613a1c3d58d673bd1a58aa21
haydarov_dev <122049418+HaydarovDev@users.noreply.github.com>
2025-08-15 18:58:17 +0500

    feat: testing

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Link to={"/auth"}>auth</Link>
+    </div>
+  );
 };
 
 export default Home;
