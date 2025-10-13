2fd4fafbdb24bd2a246b84528684911b40480654
haydarov_dev <122049418+HaydarovDev@users.noreply.github.com>
2025-10-13 15:55:39 +0500

    updated

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -1,8 +1,11 @@
+import useTheme from '../../Context/UseTheme';
 import './Loader.css';
 
 const Loader = () => {
+  const { dark } = useTheme();
+
   return (
-    <div className="loader">
+    <div className={`loader ${dark ? 'dark' : ''}`}>
       <div className="lds-spinner">
         <div></div>
         <div></div>
