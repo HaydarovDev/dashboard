1cdfc86286818332cbcdbd9fee7a85ba5eb8c577
haydarov_dev <122049418+HaydarovDev@users.noreply.github.com>
2025-10-16 22:07:12 +0500

    feat: bottom navbar added

--- client/src/components/BottomNavbar/BottomNavbar.tsx
+++ client/src/components/BottomNavbar/BottomNavbar.tsx
@@ -1,7 +1,66 @@
+import { NavLink } from 'react-router-dom';
 import './BottomNavbar.css';
+import DashboardIcon from '../../assets/images/icons/DashboardIcon';
+import MarketIcon from '../../assets/images/icons/MarketIcon';
+import MessageIcon from '../../assets/images/icons/MessageIcon';
+import WalletIcon from '../../assets/images/icons/WalletIcon';
+import ActivityIcon from '../../assets/images/icons/ActivityIcon';
+import useTheme from '../../Context/UseTheme';
 
 const BottomNavbar = () => {
-  return <nav className="bottomNav"></nav>;
+  const { dark } = useTheme();
+
+  return (
+    <nav className="bottomNav">
+      <ul>
+        <li>
+          <NavLink to={'/'} className={dark ? 'dark' : ''}>
+            {({ isActive }) => (
+              <>
+                <DashboardIcon color={isActive || dark ? 'white' : 'black'} />
+              </>
+            )}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={'market'} className={dark ? 'dark' : ''}>
+            {({ isActive }) => (
+              <>
+                <MarketIcon color={isActive || dark ? 'white' : 'black'} />
+              </>
+            )}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={'message'} className={dark ? 'dark' : ''}>
+            {({ isActive }) => (
+              <>
+                <MessageIcon color={isActive || dark ? 'white' : 'black'} />
+              </>
+            )}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={'wallet'} className={dark ? 'dark' : ''}>
+            {({ isActive }) => (
+              <>
+                <WalletIcon color={isActive || dark ? 'white' : 'black'} />
+              </>
+            )}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={'activity'} className={dark ? 'dark' : ''}>
+            {({ isActive }) => (
+              <>
+                <ActivityIcon color={isActive || dark ? 'white' : 'black'} />
+              </>
+            )}
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default BottomNavbar;
