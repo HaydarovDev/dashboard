4fcd700a2300dfbfdc69541d24a04ffea5ec28db
HaydarovDev <asadbekc0der1@gmail.com>
2025-10-29 16:59:36 +0500

    feat: message is added

--- client/src/assets/images/icons/DotsMoreIcon.tsx
+++ client/src/assets/images/icons/DotsMoreIcon.tsx
@@ -14,23 +14,23 @@ const DotsMoreIcon = () => {
       <path
         d="M13.3466 14.4578C13.9609 14.4578 14.4588 13.9599 14.4588 13.3456C14.4588 12.7314 13.9609 12.2334 13.3466 12.2334C12.7323 12.2334 12.2344 12.7314 12.2344 13.3456C12.2344 13.9599 12.7323 14.4578 13.3466 14.4578Z"
         stroke={dark ? 'white' : '#171717'}
-        stroke-width="2.66932"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.66932"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M13.3466 22.244C13.9609 22.244 14.4588 21.746 14.4588 21.1317C14.4588 20.5175 13.9609 20.0195 13.3466 20.0195C12.7323 20.0195 12.2344 20.5175 12.2344 21.1317C12.2344 21.746 12.7323 22.244 13.3466 22.244Z"
         stroke={dark ? 'white' : '#171717'}
-        stroke-width="2.66932"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.66932"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M13.3466 6.67268C13.9609 6.67268 14.4588 6.17472 14.4588 5.56046C14.4588 4.9462 13.9609 4.44824 13.3466 4.44824C12.7323 4.44824 12.2344 4.9462 12.2344 5.56046C12.2344 6.17472 12.7323 6.67268 13.3466 6.67268Z"
         stroke={dark ? 'white' : '#171717'}
-        stroke-width="2.66932"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.66932"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- client/src/components/ChatUser/ChatUser.tsx
+++ client/src/components/ChatUser/ChatUser.tsx
@@ -1,20 +1,47 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './ChatUser.css';
 import { users } from '../../service/service';
 import VideoIcon from '../../assets/images/icons/VideoIcon';
 import PhoneIcon from '../../assets/images/icons/PhoneIcon';
 import DotsMoreIcon from '../../assets/images/icons/DotsMoreIcon';
+import ArrowLeft from '../../assets/images/icons/ArrowLeft';
+import MicrophoneIcon from '../../assets/images/icons/MicrophoneIcon';
+import SendIcon from '../../assets/images/icons/SendIcon';
+import React, { useEffect, useRef, useState } from 'react';
 
 const ChatUser = () => {
   const { username } = useParams();
+  const navigate = useNavigate();
+  const chatRef = useRef<HTMLInputElement>(null);
+  const [messages, setMessages] = useState<string[]>([]);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const handleSubmit = async (e?: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e && e.key !== 'Enter') return;
+
+    const value = chatRef.current?.value ?? '';
+    if (value === '') return;
+
+    setMessages((prev) => [...prev, value]);
+
+    if (chatRef.current) {
+      chatRef.current.value = '';
+    }
+  };
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
 
   const userDetails = users.find((user) => user.username === username);
-  console.log(userDetails);
 
   return (
     <article className="chat">
       <header>
         <div className="user">
+          <button onClick={() => navigate('/message')}>
+            <ArrowLeft />
+          </button>
           {userDetails?.image === '' ? (
             <span>{userDetails?.username.slice(0, 1)}</span>
           ) : (
@@ -37,6 +64,35 @@ const ChatUser = () => {
           </button>
         </div>
       </header>
+      <div className="messages">
+        {messages &&
+          messages.map((value, index) => {
+            return (
+              <div key={index}>
+                <p>{value}</p>
+              </div>
+            );
+          })}
+        <div ref={messagesEndRef} />
+      </div>
+
+      <footer>
+        <button>
+          <MicrophoneIcon />
+        </button>
+        <label htmlFor="inputchat">
+          <input
+            type="text"
+            ref={chatRef}
+            placeholder="Message"
+            id="inputchat"
+            onKeyDown={handleSubmit}
+          />
+          <button onClick={() => handleSubmit()}>
+            <SendIcon />
+          </button>
+        </label>
+      </footer>
     </article>
   );
 };
