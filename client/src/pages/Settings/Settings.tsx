83b379755d3125e0ae1e63bab1add70411ba009f
HaydarovDev <asadbekcoder1@gmail.com>
2025-11-04 19:11:58 +0500

    feat: added editIcon and finished settings page

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -1,5 +1,6 @@
 import './Settings.css';
 import user from '../../assets/images/nft3.png';
+import EditIcon from '../../assets/images/icons/EditIcon';
 
 const Settings = () => {
   const username: string = 'Joseph Ren';
@@ -17,6 +18,40 @@ const Settings = () => {
             </div>
           </div>
         </div>
+        <form action="" className="formValidation">
+          <label htmlFor="name">
+            <input
+              type="text"
+              placeholder="Asadbek Haydarov"
+              id="name"
+              required
+            />
+            <EditIcon />
+          </label>
+          <label htmlFor="username">
+            <input
+              type="text"
+              placeholder="haydarov_dev"
+              id="username"
+              required
+            />
+            <EditIcon />
+          </label>
+          <label htmlFor="email">
+            <input
+              type="email"
+              placeholder="asadbek.haydarov.code@gmail.com"
+              id="email"
+              required
+            />
+            <EditIcon />
+          </label>
+          <label htmlFor="address">
+            <input type="text" placeholder="Navoi" id="address" required />
+            <EditIcon />
+          </label>
+          <button>Update</button>
+        </form>
       </div>
     </article>
   );
